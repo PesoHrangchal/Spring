94b744b31f2db4c36daf8b9d74e4ffda0df7bb7e
Pensionthieng Hrangchal <ezepensionhmar@gmail.com>
2023-02-15 11:34:28 +0530

    Add files via upload

---
+++ Peso_Tutorial_Management_Spring/src/test/java/com/springBoot/ptm/PesoTutorialManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.ptm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesoTutorialManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
